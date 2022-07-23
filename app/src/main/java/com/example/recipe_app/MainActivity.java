10cef7017f2f94f13e56c24257d4e6bcd1c64289
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-07-23 14:32:54 +0700

    [TESTING] username and user id with sharedpreferences

--- app/src/main/java/com/example/recipe_app/MainActivity.java
+++ app/src/main/java/com/example/recipe_app/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.recipe_app;
 
+import static com.example.recipe_app.LoginActivity.TAG_USERNAME;
+import static com.example.recipe_app.LoginActivity.my_shared_preferences;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String username, userid;
+    TextView  txt_username, txt_userid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_username = findViewById(R.id.txt_username);
+        txt_userid = findViewById(R.id.txt_password);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(my_shared_preferences, MODE_PRIVATE);
+        username = sharedPreferences.getString(TAG_USERNAME, null);
+        userid = sharedPreferences.getString("user_id", null);
+
+
+        txt_username.setText(username);
+        txt_userid.setText(userid);
     }
 }
