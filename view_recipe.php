3efb06ac859b392f1f955d2c28a5d68b009ec689
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-07-26 07:09:13 +0700

    added api to get data by likes and fix minor bugs

--- view_recipe.php
+++ view_recipe.php
@@ -3,13 +3,21 @@ include_once("connection.php");
 error_reporting(0);
 $status = $_GET['status'];
 $category = $_GET['category'];
+$likes = $_GET['likes'];
 
+// Api untuk get recipes by category
 if (!empty($category)) {
 
-    $query = "SELECT * FROM recipe WHERE status = '$status' AND category = '$category'";
-} else {
+    $query = "SELECT * FROM recipe WHERE status = '$status' AND category = '$category' order by recipe_id";
+}
+
+//Api untuk get popular recipes
+if (!empty($likes)) {
+    $query = "SELECT * FROM recipe WHERE status = '$status' AND likes > 100 order by likes desc";
+}
+// Api untuk get recipes by status
+if (is_null($category) && is_null($likes)) {
     $query = "SELECT * FROM recipe where status = '$status'";
-    // echo "<script>alert('Please select a category');</script>";
 }
 // $query = "SELECT * FROM recipe WHERE status = '$status'";
 $result = mysqli_query($koneksi, $query);
