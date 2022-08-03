6b62de4c796106ad2f92452c027276bda997dfb8
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-08-04 05:03:36 +0700

    add get comment API

---
+++ get_comment.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once("connection.php");
+
+$recipe_id = $_GET['recipe_id'];
+
+
+$query = "SELECT c.comment_id, c.recipe_id, c.user_id, c.comment, c.comment_date, c.comment_time,
+    u.username, u.photo_profile, u.email from comments c, users u where c.user_id= u.user_id and c.recipe_id='$recipe_id'";
+
+$result = mysqli_query($koneksi, $query);
+$arraydata = array();
+
+while ($baris = mysqli_fetch_assoc($result)) {
+    $arraydata[] = $baris;
+}
+echo json_encode($arraydata);
