1e4e8202f832846eba2e76cc9e1e573f2d09f0ce
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-08-05 05:28:32 +0700

    add post_comment api

---
+++ post_comment.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once("connection.php");
+
+class emp
+{
+}
+
+date_default_timezone_set('Asia/Jakarta');
+$recipe_id = $_POST['recipe_id'];
+$user_id = $_POST['user_id'];
+$comment = $_POST['comment'];
+$upload_date = date("Y-m-d");
+$upload_time = date("H:i:s");
+
+
+$query = "INSERT INTO comments (recipe_id, user_id, comment, comment_date, comment_time) VALUES ('$recipe_id', '$user_id', '$comment', '$upload_date', '$upload_time')";
+$result = mysqli_query($koneksi, $query);
+
+if ($result) {
+    $response = new emp();
+    $response->success = 1;
+    $response->message = "Successfully uploaded";
+    die(json_encode($response));
+} else {
+    $response = new emp();
+    $response->success = 0;
+    $response->message = "Error while uploading";
+    die(json_encode($response));
+}
