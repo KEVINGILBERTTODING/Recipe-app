a61d8f0c64788e23da26d180a401b72b205972ce
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-08-07 17:45:31 +0700

    add get profile api

---
+++ get_profile.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'connection.php';
+
+$user_id = $_GET['user_id'];
+
+$query = "SELECT * FROM users WHERE user_id = '$user_id'";
+$result = mysqli_query($koneksi, $query);
+$arraydata = array();
+
+while ($baris = mysqli_fetch_assoc($result)) {
+    $arraydata[] = $baris;
+}
+echo json_encode($arraydata);
