a2c6547e93d88fce9c5cc41c9ddf1a49577997c5
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-08-22 05:53:24 +0700

    fix

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php
 include_once "connection.php";
-class usr{}
+class usr
+{
+}
 
 $email = $_POST["email"];
 $username = $_POST["username"];
@@ -12,19 +14,17 @@ $time = $_POST["time"];
 
 $query = mysqli_query($koneksi, "INSERT INTO users(`email`, `username`, `password`, `date`, `time`) values('$email', '$username', '$password', '$date', '$time')");
 
-if ($query){
- $response = new usr();
- $response->success = 1;
- $response->message = "Welcome".$username;
- $response->username = $username;
- die(json_encode($response));
-
+if ($query) {
+    $response = new usr();
+    $response->success = 1;
+    $response->message = "Welcome " . $username;
+    $response->username = $username;
+    die(json_encode($response));
 } else {
- $response = new usr();
- $response->success = 0;
- $response->message = "Gagal Mendaftar";
- die(json_encode($response));
+    $response = new usr();
+    $response->success = 0;
+    $response->message = "Failed";
+    die(json_encode($response));
 }
 
 mysqli_close($con);
-?> 
