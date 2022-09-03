49c3e184267d891f321438bd1b9cdd5b27a6b758
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-09-03 14:28:14 +0700

    make app version splashscreen id dynamic

--- app/src/main/java/com/example/recipe_app/SplashScreen.java
+++ app/src/main/java/com/example/recipe_app/SplashScreen.java
@@ -6,12 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import com.example.recipe_app.Admin.Interface.InterfaceAdmin;
+import com.example.recipe_app.Model.AppModel;
+import com.example.recipe_app.Util.DataApi;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SplashScreen extends AppCompatActivity {
+
+    TextView tv_app_version;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        tv_app_version = findViewById(R.id.tv_app_version);
 
         hideNavigationBar();
 
@@ -23,6 +41,25 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
             }
         }, 3000L);
+
+
+        // get app version
+        InterfaceAdmin interfaceAdmin = DataApi.getClient().create(InterfaceAdmin.class);
+        interfaceAdmin.viewAbout().enqueue(new Callback<List<AppModel>>() {
+            @Override
+            public void onResponse(Call<List<AppModel>> call, Response<List<AppModel>> response) {
+                List<AppModel> appModelList  = response.body();
+                if (response.body().size() > 0 ) {
+                    tv_app_version.setText(appModelList.get(0).getApp_version());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<AppModel>> call, Throwable t) {
+
+            }
+        });
 
     }
 
